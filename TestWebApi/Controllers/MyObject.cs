a77570ee26c6931428daa55b80cfacfbcb20de0d
meddlin <drushing.dev@gmail.com>
2019-04-24 20:53:33 -0500

    pushing it all up

---
+++ TestWebApi/Controllers/MyObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApi.Controllers
+{
+    public class MyObject
+    {
+        public string Value { get; set; }
+        public string AnotherValue { get; set; }
+    }
+}
